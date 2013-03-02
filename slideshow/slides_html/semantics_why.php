b1e92cf1206e1f8c72fb4927f616c16a22f41127
Karl Stanton <karl@niceproduce.com>
2013-03-02 15:30:45 -0500

    Fixed permissions and added more HTML slides

---
+++ slideshow/slides_html/semantics_why.php
@@ -0,0 +1,8 @@
+		<div class="slide">
+			<h4>Why is semantic markup important?</h4>
+			<p>Think of your website like you would a newspaper</p>
+			<p>
+			<img src="images/html_semantics_paper.jpg" alt="html_semantics_paper" height="700" style="width: 400px; display: block; margin: 0 auto;" />
+			</p>
+
+		</div>
