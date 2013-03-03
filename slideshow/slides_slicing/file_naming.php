59173f06b63cf3c773a8e7d2680fcae351cd0635
Kate Proulx <kathrynproulx@gmail.com>
2013-03-03 15:28:50 -0500

    kate is done

--- slideshow/slides_slicing/file_naming.php
+++ slideshow/slides_slicing/file_naming.php
@@ -1,12 +1,21 @@
 		<div class="slide">
-			<h4>File naming & Put it in /images folder</h4>
+			<h4>Saving to the /images folder...</h4>
 
-			<ul class="build">
-				<li>lorem ipsum</li>
-				<li>lorem ipsum</li>
-				<li>lorem ipsum</li>
-				<li>lorem ipsum</li>
-			</ul>
+			<ol class="build">
+				<li>In the 'Save for Web...' window click 'Save...'</li>
+				<li>Navigate to your project folder</li>
+				<li>Click 'New Folder' and name the folder <i>images</i></li>
+				<li><strong>Save all the image files for your project here!</strong></li>
+			</ol>
+
+
+			<h4>& a few notes on file naming conventions...</h4>
+
+			<ol class="build">
+				<li><strong>No spaces!</strong> Use-dashes-instead.</li>
+				<li><strong>No symbols!</strong> Letters, numbers, dashes, & underscores&nbsp;only.
+				<li>Make file names descriptive, using keyword-rich words.</li>
+			</ol>
 			
 		</div>
 		
