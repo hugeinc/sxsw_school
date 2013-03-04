88681b88c528f1f6857fb699e19799eee6312487
Katie Sexton <ksexton@hugeinc.com>
2013-03-04 15:36:44 -0500

    Actually add files

---
+++ slideshow/slides_css/media_queries.php
@@ -0,0 +1,6 @@
+<div class="slide">
+	<h4>Media Queries.</h4>
+	<p>Consists of a media type (such as screen, print, device) and various css rules to be applied.</p>
+	<p>Allows a developer to target either devices or size-based breakpoints (or both) to customize a website's layout and appearance.</p>
+	<p><strong>Media queries are one very important tool for creating a responsive design.</strong></p>	
+</div>
