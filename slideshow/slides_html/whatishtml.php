26b091a9c10d4cfa5f42d7d53acf0131d4599059
Jesus Castellanos <jcastellanos@hugeinc.com>
2013-03-05 18:12:28 -0800

    added a slide in html to create new document

--- slideshow/slides_html/whatishtml.php
+++ slideshow/slides_html/whatishtml.php
@@ -1,14 +1,13 @@
-		<div class="slide">
-			<h4>What is HTML?</h4>
+<div class="slide">
+	<h4>What is HTML?</h4>
 
-			<ul class="build">
-				<li>HTML stands for Hyper Text Markup Language</li>
-				<li>It is a language that was invented soley for creating websites</li>
-				<li>HTML is built by using a series of <strong>tags</strong></li>
-				<li>HTML can be read by search engines</li>
-				<li>HTML is generally referred to by developers as <strong>markup</strong></li>
-				<li>Without HTML, there is no World Wide Web!</li>
-			</ul>
+	<ul class="build">
+		<li>HTML stands for Hyper Text Markup Language</li>
+		<li>It is a language that was invented soley for creating websites</li>
+		<li>HTML is built by using a series of <strong>tags</strong></li>
+		<li>HTML can be read by search engines</li>
+		<li>HTML is generally referred to by developers as <strong>markup</strong></li>
+		<li>Without HTML, there is no World Wide Web!</li>
+	</ul>
 
-		</div>
-		
+</div>
