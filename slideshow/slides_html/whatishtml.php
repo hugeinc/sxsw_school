c5741146946514fd367cdbd17d8888a78ced8281
Liza Detrick <ldetrick@hugeinc.com>
2013-03-06 17:26:32 -0800

    spelling errors

--- slideshow/slides_html/whatishtml.php
+++ slideshow/slides_html/whatishtml.php
@@ -3,7 +3,7 @@
 
 	<ul class="build">
 		<li>HTML stands for Hyper Text Markup Language</li>
-		<li>It is a language that was invented soley for creating websites</li>
+		<li>It is a language that was invented solely for creating websites</li>
 		<li>HTML is built by using a series of <strong>tags</strong></li>
 		<li>HTML can be read by search engines</li>
 		<li>HTML is generally referred to by developers as <strong>markup</strong></li>
